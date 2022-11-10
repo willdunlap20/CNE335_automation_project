1ac93bb4b9953547c6b513ba0a9a93514f445e0d
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2022-11-10 08:34:57 -0800

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,10 @@
+class Server:
+    """ Server class for representing and manipulating servers. """
+
+    def __init__(self, server_ip):
+        # TODO -
+        self.server_ip = server_ip
+
+    def ping(self):
+        # TODO - Use os module to ping the server
+        return
---
+++ main.py
@@ -0,0 +1,13 @@
+# This is the template code for the CNA337 Final Project
+# Zachary Rubin, zrubin@rtc.edu
+# CNA 337 Fall 2020
+
+def print_program_info():
+    # TODO - Change your name
+    print("Server Automator v0.1 by Your Name")
+
+# This is the entry point to our program
+if __name__ == '__main__':
+    print_program_info()
+    # TODO - Create a Server object
+    # TODO - Call Ping method and print the results
