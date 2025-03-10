4f4aa1f2b6bb2329fc126753dc9a1dce4e7c94fa
willdunlap20 <willdunlapb@gmail.com>
2025-03-10 15:34:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# Will Dunlap
+# CNE 335 Winter
+from server import Server
 
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Will Dunlap")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+    server = Server("52.38.252.235")
+    response = server.ping()
+    print("Ping response:", response)
